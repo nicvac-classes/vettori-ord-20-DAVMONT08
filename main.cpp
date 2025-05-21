21f8887645288d0fa64f27f6fe274e40bc7dc866
DAVMONT08 <davide.montella@marconibari.edu.it>
2025-05-21 22:00:05 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -2,10 +2,84 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+               
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int N, i, j, z, t, c;
+
+    cout << "Inserire il numero di listelli a disposizione" << endl;
+    cin >> N;
+
+    int L[N];
+
+    
+    srand(time(0));
+
+   
+    i = 0;
+    while (i < N) {
+        L[i] = 4 + rand() % 7; 
+        cout << "Lunghezza " << i + 1 << "° listello: " << L[i] << endl;
+        i++;
+    }
+
+    
+    i = 0;
+    while (i < N) {
+        j = 0;
+        while (j < (N - 1) - i) {
+            if (L[j] > L[j + 1]) {
+                t = L[j];
+                L[j] = L[j + 1];
+                L[j + 1] = t;
+            }
+            j++;
+        }
+        i++;
+    }
+
+    
+    cout << "Lunghezza listelli ordinati:" << endl;
+    for (i = 0; i < N; i++) {
+        cout << L[i] << endl;
+    }
+
+    
+    c = 0;
+    if (N >= 4) {
+        i = 0;
+        while (i < N - 1) {
+            z = i + 1;
+            if (L[i] == L[z]) {
+                t = 2;
+                while (z + 1 < N && L[z] == L[z + 1]) {
+                    t++;
+                    z++;
+                }
+                if (t >= 4) {
+                    c++;
+                }
+                i = z + 1;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    cout << "Numero di gruppi di 4 listelli uguali: " << c << endl;
+
+    
+}
+
+
+
+
+
+
+
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
